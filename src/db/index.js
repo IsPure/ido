311c0526e04724dc5c078075e8f4037576c6eb0d
Isaac Pure <isaacpure@gmail.com>
2023-01-23 18:31:41 -0600

    initial commit

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+    user: 'isaacpure',
+    host: 'localhost',
+    database: 'pern_auth',
+    password: '',
+    port: 5432,
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text, params)
+}
---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const { PORT } = require('./constants') 
+const cookieParser = require('cookie-parser')
+
+//initialize middlewares
+app.use(express.json())
+app.use(cookieParser())
+
+//import routes
+const authRoutes = require('./routes/auth')
+
+//initialize routes
+app.use ('/api', authRoutes)
+
+//app start
+const appStart = () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`App listening/running on port ${PORT}`)
+    })
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
+}
+
+appStart()
